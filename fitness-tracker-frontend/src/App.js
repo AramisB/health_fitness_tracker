19be3558acf30d9b1628090cf0641c5fd191baec
AramisB <brendachem7@gmail.com>
2024-10-16 16:16:59 +0300

    DigitalOcean's Monorepo Configuration with .yaml

--- fitness-tracker-frontend/src/App.js
+++ fitness-tracker-frontend/src/App.js
@@ -8,7 +8,7 @@ import ExerciseLog from './components/ExerciseLog';
 import Goals from './components/Goals';
 import Progress from './components/Progress';
 import Home from './components/Home';
-import ProtectedRoute from './components/ProtectedRoute'; // Import ProtectedRoute
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
